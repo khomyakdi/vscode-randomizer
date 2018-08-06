77939114cb7c497fd1c101ae3b7294cbab1c8d22
Khomiak Dmytro <khomyakdi@gmail.com>
2018-08-06 19:16:38 +0300

    added ignoring empty lines in multyline selection, removed and commented console.logs, format code

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,8 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-randomizer" is now active!');
+
+    // console.log('Congratulations, your extension "vscode-randomizer" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
@@ -19,53 +20,60 @@ export function activate(context: vscode.ExtensionContext) {
     let getRandom = vscode.commands.registerCommand('extension.getRandom', () => {
         let editor = vscode.window.activeTextEditor;
 
-        if(!editor){
+        if (!editor) {
             vscode.window.showInformationMessage("Please open any editor window");
             return;
         };
         let selection = editor.selection;
-        let selectionRange = new vscode.Range(selection.start,selection.end);
-        let publish = (result:any) => {
-            let ed =vscode.window.activeTextEditor;
-            function edit(e:TextEditor){
+        let selectionRange = new vscode.Range(selection.start, selection.end);
+        let publish = (result: any) => {
+            let ed = vscode.window.activeTextEditor;
+            function edit(e: TextEditor) {
                 e.edit(
-                    function(edit){
-                    edit.replace(selectionRange,result+"");
-                });
+                    function (edit) {
+                        edit.replace(selectionRange, result + "");
+                    });
             }
             edit(ed);
         }
-        if(!selection.isSingleLine){
+        if (!selection.isSingleLine) {
             let selectedMultilines = editor.document.getText(selection).split('\n');
+
+            let selectedMultilinesWithoutEmpty = [];
+            selectedMultilines.map((line) => {
+                if (line.replace(/\s+/g, '') != '')
+                    selectedMultilinesWithoutEmpty.push(line);
+            });
+            selectedMultilines = selectedMultilinesWithoutEmpty;
+
             let lineCount = selectedMultilines.length;
-            publish(selectedMultilines[Randomizer.getRandomNumOnRange(0,lineCount-1)]);
+            publish(selectedMultilines[Randomizer.getRandomNumOnRange(0, lineCount - 1)]);
             return;
         }
-        let selectedText = editor.document.getText(selection[0]).trim().replace(/\s+/g,' ');
+        let selectedText = editor.document.getText(selection).trim().replace(/\s+/g, ' ');
         let result;
 
-        if(selectedText === ""){
+        if (selectedText === "") {
             vscode.window.showInformationMessage("Please select some text");
             return;
         }
         switch (selectedText) {
-            case "rgb":{
+            case "rgb": {
                 result = Randomizer.getRandomRGB();
                 publish(result);
-            }break;
-        
-            default:{
+            } break;
+
+            default: {
                 let range = selectedText.split(' ');
-               
-                let cond =(isNaN(+range[0]) || isNaN(+range[1]));
-                // console.log(`range ${range}; cond: ${cond} all:${range.length!=2 || cond}`);
-                if (range.length!=2 || cond) {
-                    vscode.window.showInformationMessage('Selected text should be "number number" format\n Example: "1 10"' );
+
+                let cond = (isNaN(+range[0]) || isNaN(+range[1]));
+                if (range.length != 2 || cond) {
+                    vscode.window.showInformationMessage('Selected text should be like one of the template: "number number" or "rgb" or multiline selection');
                     break;
                 }
-                result = Randomizer.getRandomNumOnRange(parseInt(range[0]),parseInt(range[1]));
+                result = Randomizer.getRandomNumOnRange(parseInt(range[0]), parseInt(range[1]));
                 publish(result);
-            }break;
+            } break;
         }
 
     });
